7d60eb19bad803f8f91575e24a9069b44278bb16
Marino Kolaric <marinokolaric@gmail.com>
2020-09-11 18:51:53 +0200

    Add Favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
         background_color: `#453399`,
         theme_color: `#663119`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     {
