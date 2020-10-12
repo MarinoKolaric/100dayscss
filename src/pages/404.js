d92e496211fd31dcc0870efc4f45bcfcb3624762
Marino Kolaric <marinokolaric@gmail.com>
2020-10-12 23:57:19 +0200

    Add 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,57 @@
 import React from 'react';
+import styled from 'styled-components';
+import { LayoutDay, SEO, GoBack } from '@components';
+import { theme } from '@style';
 
-import { Layout, SEO } from '@components';
+const Container = styled.div`
+  background-color: #222;
+  height: 400px;
+  position: relative;
+  width: 100%;
+
+  ${theme.mediaQueries('mobileLarge')`
+    width: 400px;
+  `}
+`;
+
+const Svg = styled.svg``;
+
+const SvgText = styled.text`
+  font-size: 80rem;
+  font-weight: 600;
+  text-transform: uppercase;
+`;
 
 const NotFoundPage = () => (
-  <Layout>
+  <LayoutDay>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+    <Container>
+      <Svg width="100%" height="100%">
+        <defs>
+          <linearGradient id="rainbow" x1="0" x2="0" y1="0" y2="100%">
+            <stop stop-color="#FF5B99" offset="0%" />
+            <stop stop-color="#FF5447" offset="20%" />
+            <stop stop-color="#FF7B21" offset="40%" />
+            <stop stop-color="#EAFC37" offset="60%" />
+            <stop stop-color="#4FCB6B" offset="80%" />
+            <stop stop-color="#51F7FE" offset="100%" />
+          </linearGradient>
+        </defs>
+        <SvgText
+          x="50%"
+          y="30%"
+          fill="url(#rainbow)"
+          textAnchor="middle"
+          dominantBaseline="middle"
+        >
+          <tspan x="50%" dy="0">day</tspan>
+          <tspan x="50%" dy="80">not</tspan>
+          <tspan x='50%' dy="80">found</tspan>
+        </SvgText>
+      </Svg>
+    </Container>
+    <GoBack />
+  </LayoutDay>
 );
 
 export default NotFoundPage;
