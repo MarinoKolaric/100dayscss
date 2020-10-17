38754d4aa9f6fc821c0335f96253e19c15b39ca6
Marino Kolaric <marinokolaric@gmail.com>
2020-10-17 12:39:03 +0200

    Add local fonts

---
+++ src/style/fonts.js
@@ -0,0 +1,5 @@
+import ElevateEOT from './fonts/Elevate.ttf.eot';
+import ElevateSVG from './fonts/Elevate.ttf.svg';
+import ElevateWOFF from './fonts/Elevate.ttf.woff';
+
+export default { ElevateEOT, ElevateSVG, ElevateWOFF };
---
+++ src/style/fonts/index.js
@@ -0,0 +1,5 @@
+import ElevateEOT from 'Elevate.ttf.eot';
+import ElevateSVG from '.Elevate.ttf.svg';
+import ElevateWOFF from 'Elevate.ttf.woff';
+
+export default { ElevateEOT, ElevateSVG, ElevateWOFF };
